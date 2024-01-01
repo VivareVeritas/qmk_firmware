b0aebb6ca20ce27566f92087c6da7d8763de00ac
Vivare <vivareveritas@gmail.com>
2024-01-01 04:22:55 -0500

    Added my charybdis files

--- keyboards/bastardkb/charybdis/4x6/keymaps/VivareVeritas/config.h
+++ keyboards/bastardkb/charybdis/4x6/keymaps/VivareVeritas/config.h
@@ -1,5 +1,4 @@
-/*
- * Copyright 2021 Quentin LEBASTARD <qlebastard@gmail.com>
+/**
  * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -15,17 +14,26 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
 
-/* Trackball angle adjustment. */
-#define ROTATIONAL_TRANSFORM_ANGLE -25
+/*Vivare Changes*/
+#define TAPPING_TERM 170
+// !BAD >> #define HOLD_ON_OTHER_KEY_PRESS
+//#define PERMISSIVE_HOLD
 
+#ifdef VIA_ENABLE
+/* VIA configuration. */
+#    define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#endif // VIA_ENABLE
 
-/* RGB SETTINGS v */
-#ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_LED_COUNT 58
-#    define RGB_MATRIX_SPLIT \
-        { 29, 29 }
-#endif
+/* Disable unused features. */
+#define NO_ACTION_ONESHOT
 
+/* Charybdis-specific features. */
+
+#ifdef POINTING_DEVICE_ENABLE
+// Automatically enable the pointer layer when moving the trackball.  See also:
+// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS`
+// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD`
+// #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+#endif // POINTING_DEVICE_ENABLE
---
+++ keyboards/bastardkb/charybdis/4x6/keymaps/manna-harbour_miryoku/rules.mk
@@ -0,0 +1,9 @@
+# Copyright 2021 Manna Harbour
+# https://github.com/manna-harbour/miryoku
+
+MIRYOKU_KLUDGE_THUMBCOMBOS=yes
+
+EXTRAFLAGS += -flto
+MOUSEKEY_ENABLE = no
+AUTO_SHIFT_ENABLE = no
+RGB_MATRIX_ENABLE=no
