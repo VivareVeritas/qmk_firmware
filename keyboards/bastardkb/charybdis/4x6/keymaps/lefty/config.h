343bafe7891dadf091587fac2ed20e5abfb17b95
Vivare <vivareveritas@gmail.com>
2024-01-03 14:33:39 -0500

    made a lefty keymap

---
+++ keyboards/bastardkb/charybdis/4x6/keymaps/lefty/config.h
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+/*Vivare Changes*/
+
+/* Disable unused features. */
+#define NO_ACTION_ONESHOT
+#define NO_MUSIC_MODE
+#define NO_ACTION_TAPPING
+#define NO_CHARYBDIS_KEYCODES
+#define NO_MUSIC_MODE
+#define NO_ACTION_LAYER
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
